a54d7d1810c92231f20cbd0afdd4fc009c3972f4
Vic <vic@minustime.com>
2018-06-04 20:39:16 -0400

    Add mobx test

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,23 @@
-function test() {
-  console.log('this is a test!');
+import axios from 'axios';
+
+function fetchContent(url) {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(url)
+      .then(response => resolve(response.data))
+      .catch(reject);
+  });
+}
+
+function fetchPlans() {
+  return fetchContent('http://www.mocky.io/v2/5b15a67b3200007d00b8a459');
+}
+
+function fetchUser() {
+  return fetchContent('http://www.mocky.io/v2/5b15baa93200000f00b8a499');
 }
 
 export default {
-  test,
+  fetchPlans,
+  fetchUser,
 };
