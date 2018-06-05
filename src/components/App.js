93f18cbf6ec216c30eb7c967f716af24e6657f78
Vic <vic@minustime.com>
2018-06-05 13:16:43 -0400

    Export App component as observer

--- src/components/App.js
+++ src/components/App.js
@@ -4,37 +4,35 @@ import { observer } from 'mobx-react';
 import Plans from './Plans';
 import User from './User';
 
-const App = observer(
-  class App extends React.Component {
-    renameUser = () => {
-      this.props.store.changeUserName(`Random ${Date.now()}`);
-    };
+class App extends React.Component {
+  renameUser = () => {
+    this.props.store.changeUserName(`Random ${Date.now()}`);
+  };
 
-    handleGetPlansClick = () => {
-      this.props.store.fetchPlans();
-    };
+  handleGetPlansClick = () => {
+    this.props.store.fetchPlans();
+  };
 
-    loadPlans = () => {
-      this.props.store.loadPlans();
-    };
+  loadPlans = () => {
+    this.props.store.loadPlans();
+  };
 
-    addPlan = () => {
-      this.props.store.addPlan();
-    };
+  addPlan = () => {
+    this.props.store.addPlan();
+  };
 
-    render() {
-      return (
-        <div>
-          <User profile={this.props.store.user} renameUser={this.renameUser} />
-          <Plans
-            list={this.props.store.plans}
-            loadPlans={this.loadPlans}
-            addPlan={this.addPlan}
-          />
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+        <User profile={this.props.store.user} renameUser={this.renameUser} />
+        <Plans
+          list={this.props.store.plans}
+          loadPlans={this.loadPlans}
+          addPlan={this.addPlan}
+        />
+      </div>
+    );
   }
-);
+}
 
-export default App;
+export default observer(App);
